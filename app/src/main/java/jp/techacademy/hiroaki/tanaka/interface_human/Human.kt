359e5b172f6f9108f7c4aa94a843e3c811110b76
hr-tanaka <hr-tanaka@kddi-tech.com>
2022-08-13 16:09:23 +0900

    Humanクラスのコンストラクタの引数を３つにして、hobbyを設定できるように修正 MainActivityでHumanクラスのインスタンスを2つ作成

--- app/src/main/java/jp/techacademy/hiroaki/tanaka/interface_human/Human.kt
+++ app/src/main/java/jp/techacademy/hiroaki/tanaka/interface_human/Human.kt
@@ -4,16 +4,14 @@ import android.util.Log
 
 open class Human: Animal, Thinkable {
 
-    var hobby: String
 
     // 引数付きコンストラクタ
-    constructor(name: String, age: Int): super(name, age) {
-    this.hobby = "音楽"
+    constructor(name: String, age: Int, hobby: String): super(name, age, hobby) {
     }
 
     // Animalクラスのメソッドをオーバーライド
     override fun say() {
-        Log.d("kotlintest", "私の名前は" + this.name + "です。年は" +  this.age + "歳です。)")
+        Log.d("kotlintest", "私の名前は" + this.name + "です。年は" +  this.age + "歳です。")
     }
     // Thinkable インターフェイスのメソッドをオーバーライド
     override fun think() {
--- app/src/main/java/jp/techacademy/hiroaki/tanaka/interface_human/MainActivity.kt
+++ app/src/main/java/jp/techacademy/hiroaki/tanaka/interface_human/MainActivity.kt
@@ -3,16 +3,21 @@ package jp.techacademy.hiroaki.tanaka.interface_human
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import jp.techacademy.hiroaki.tanaka.interface_human.Human
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val human = Human("モーツアルト", 35)
+        val human1 = Human("モーツアルト", 35, "音楽")
 
-        human.say()
-        human.think()
+        human1.say()
+        human1.think()
 
+        val human2 = Human("カールルイス", 24, "陸上")
+
+        human2.say()
+        human2.think()
     }
 }
